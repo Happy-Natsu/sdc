3617096cf04b043bdbde5564f51130653ed4c9b4
Happy-Natsu <73634185+Happy-Natsu@users.noreply.github.com>
2022-12-24 21:45:55 +0530

    Add files via upload

---
+++ SDC/main.js
@@ -0,0 +1,93 @@
+const carCanvas = document.getElementById("carCanvas");
+carCanvas.width = 200;
+const networkCanvas = document.getElementById("networkCanvas");
+networkCanvas.width = 300;
+
+const carCtx = carCanvas.getContext("2d");
+const networkCtx = networkCanvas.getContext("2d");
+
+const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
+let N = 1000;
+const cars = generateCars(N);
+
+let bestCar = cars[0];
+if (localStorage.getItem("bestBrain")) {
+  for (let i = 0; i < cars.length; i++) {
+    cars[i].brain = JSON.parse(localStorage.getItem("bestBrain"));
+    if (i != 0) {
+      NeuralNetwork.mutate(cars[i].brain, 0.1);
+    }
+  }
+}
+
+const traffic = [
+  new Car(road.getLaneCentre(1), -100, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCentre(0), -300, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCentre(2), -300, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCentre(1), -500, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCentre(2), -500, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCentre(0), -700, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCentre(1), -800, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCentre(0), -900, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCentre(2), -1000, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCentre(1), -1000, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCentre(2), -1100, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCentre(0), -1200, 30, 50, "DUMMY", 2),
+];
+
+for (let i = 0; i < traffic.length; i++) {
+  traffic[i].update(road.borders, []);
+}
+for (let i = 0; i < cars.length; i++) {
+  cars[i].update(road.borders, traffic);
+}
+for (let i = 0; i < cars.length; i++) {
+  cars[i].draw(carCtx, "blue");
+}
+
+animate();
+
+function save() {
+  localStorage.setItem("bestBrain", JSON.stringify(bestCar.brain));
+}
+
+function discard() {
+  localStorage.removeItem("bestBrain");
+}
+
+function generateCars(N) {
+  const cars = [];
+  for (let i = 1; i <= N; i++) {
+    cars.push(new Car(road.getLaneCentre(1), 100, 30, 50, "AI"));
+  }
+  return cars;
+}
+
+function animate(time) {
+  for (let i = 0; i < traffic.length; i++) {
+    traffic[i].update(road.borders, []);
+  }
+  for (let i = 0; i < cars.length; i++) {
+    cars[i].update(road.borders, traffic);
+  }
+  bestCar = cars.find((c) => c.y == Math.min(...cars.map((c) => c.y)));
+
+  carCanvas.height = window.innerHeight;
+  networkCanvas.height = window.innerHeight;
+  carCtx.save();
+  carCtx.translate(0, -bestCar.y + carCanvas.height * 0.7);
+  road.draw(carCtx);
+  for (let i = 0; i < traffic.length; i++) {
+    traffic[i].draw(carCtx, "purple");
+  }
+  carCtx.globalAlpha = 0.2;
+  for (let i = 0; i < cars.length; i++) {
+    cars[i].draw(carCtx, "blue");
+  }
+  carCtx.globalAlpha = 1;
+  bestCar.draw(carCtx, "blue", true);
+  carCtx.restore();
+  networkCtx.lineDashOffset = -time / 50;
+  Visualizer.drawNetwork(networkCtx, bestCar.brain);
+  requestAnimationFrame(animate);
+}
